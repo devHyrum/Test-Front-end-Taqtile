5562002696ceb370f53b16a5ed291ae3cc59e960
devHyrum <holivera0730@gmail.com>
2025-01-09 19:08:43 -0300

    feature/layout-Login-changed

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,11 +5,17 @@ const Login: React.FC = () => {
   return (
     <>
       <form className='container-login'>
-        <label htmlFor='uname'>E-mail</label>
-        <input type='text' placeholder='email@exemplo.com' name='uname' required />
-        <label htmlFor='psw'>Senha</label>
-        <input type='password' placeholder='*****' name='psw' required />
-        <button type='submit'>Entrar</button>
+        <div className='box-email'>
+          <label htmlFor='email'>E-mail</label>
+          <input type='text' placeholder='email@exemplo.com' name='uname' required />
+        </div>
+        <div className='box-password'>
+          <label htmlFor='password'>Senha</label>
+          <input type='password' placeholder='*****' name='psw' required />
+        </div>
+        <div className='box-submit'>
+          <button type='submit'>Entrar</button>
+        </div>
       </form>
     </>
   );
