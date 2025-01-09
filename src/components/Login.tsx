0ca277e2ecc68065239b3b17313e19b44e29fa01
devHyrum <holivera0730@gmail.com>
2025-01-09 15:18:25 -0300

    feature/login-validations

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [errorEmail, setErrorEmail] = useState<string | null>(null);
+  const [senha, setSenha] = useState<string>('');
+  const [errorSenha, setErrorSenha] = useState<string | null>(null);
+
+  const validateEmail = (): boolean => {
+    if (!email) {
+      setErrorEmail('O campo de e-mail é obrigatório');
+      return false;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setErrorEmail('Por favor, insira um e-mail válido.');
+      return false;
+    }
+    setErrorEmail(null);
+    return true;
+  };
+  const validateSenha = (): boolean => {
+    if (!senha) {
+      setErrorSenha('O campo senha é obrigatorio');
+      return false;
+    }
+    if (senha.length < 7) {
+      setErrorSenha('A senha deve ser de mínimo 7 caracteres');
+      return false;
+    }
+    const senhaRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])/;
+    if (!senhaRegex.test(senha)) {
+      setErrorSenha('A senha deve ter pelo menos um dígito e uma letra');
+      return false;
+    }
+    setErrorSenha(null);
+    return true;
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    validateEmail();
+    validateSenha();
+    if (validateEmail() && validateSenha()) {
+      console.log('E-mail e senha válidos:', email, senha);
+    } else {
+      console.log('E-mail:', email);
+      console.log('Senha:', senha);
+    }
+  };
+
   return (
     <>
       <form className='container-login'>
+      <form className='container-login' onSubmit={handleSubmit}>
         <div className='box-email'>
-          <label htmlFor='email'>E-mail</label>
-          <input type='text' placeholder='email@exemplo.com' name='email' required />
+          <label htmlFor='uname'>E-mail</label>
+          <input placeholder='email@exemplo.com' name='uname' value={email} onChange={(e) => setEmail(e.target.value)} />
+          {errorEmail && <p className='error-email'>{errorEmail}</p>}
         </div>
         <div className='box-password'>
-          <label htmlFor='password'>Senha</label>
-          <input type='password' placeholder='*****' name='password' required />
+          <label htmlFor='psw'>Senha</label>
+          <input type='password' placeholder='*****' name='psw' onChange={(e) => setSenha(e.target.value)} />
+          {errorSenha && <p className='error-senha'>{errorSenha}</p>}
         </div>
         <div className='box-submit'>
           <button type='submit'>Entrar</button>
