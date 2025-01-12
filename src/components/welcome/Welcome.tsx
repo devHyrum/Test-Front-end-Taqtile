f0afdb78dbdea1db15df11808695c2f81689254a
devHyrum <holivera0730@gmail.com>
2025-01-12 15:24:14 -0300

    feature/users-list-ui

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -2,8 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './Welcome.css';
 import { useNavigate } from 'react-router-dom';
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 const Welcome: React.FC = () => {
   const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,6 +20,13 @@ const Welcome: React.FC = () => {
       return;
     } else {
       const timeout = setTimeout(() => {
+        setUsers([
+          { id: 1, name: 'João Da Silva', email: 'joaodasilva@gmail.com' },
+          { id: 2, name: 'Maria Araujo', email: 'mariaaraujo@gmail.com' },
+          { id: 3, name: 'Francisco Lima', email: 'franciscolima@gmail.com' },
+          { id: 4, name: 'Waldir Jose Vieira', email: 'waldirvieira@gmail.com' },
+          { id: 5, name: 'Jonatan Moises Olivera Fernandez', email: 'jonatanolivera@gmail.com' },
+        ]);
         setLoading(false);
       }, 2000);
 
@@ -20,6 +34,23 @@ const Welcome: React.FC = () => {
     }
   }, [navigate]);
 
-  return <>{loading ? <div className='custom-loader-page'></div> : <div>Welcome</div>}</>;
+  return (
+    <>
+      {loading ? (
+        <div className='custom-loader-page'></div>
+      ) : (
+        <main>
+          <h1> Lista de Usuarios</h1>
+          <ul className='user-list'>
+            {users.map((user) => (
+              <li key={user.id} className='user-item'>
+                <span className='user-name'>{user.name}</span> - <span className='user-email'>{user.email}</span>
+              </li>
+            ))}
+          </ul>
+        </main>
+      )}
+    </>
+  );
 };
 export default Welcome;
