921e650cefaf9d7ee3e1371c63b4e3bf01edca47
devHyrum <holivera0730@gmail.com>
2025-01-13 21:57:27 -0300

    feature/organization-in-the-code

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Welcome: React.FC = () => {
+  return (
+    <>
+      <h1>Site Instaq</h1>
+      <p>Home Page</p>
+    </>
+  );
+};
+export default Welcome;
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_MUTATION } from '../../graphql/mutations';
-import { validateEmail, validatePassword } from '../../utils/validators';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
+import { validateEmail, validatePassword } from '../../utils/validators';
+
 
 const Login: React.FC = () => {
   const [formState, setFormState] = useState({
@@ -17,14 +19,18 @@ const Login: React.FC = () => {
     }));
   };
 
+  const navigate = useNavigate();
+
   const [login, { loading, error }] = useMutation(LOGIN_MUTATION, {
     onCompleted: (data) => {
+
       const token = data?.login?.token;
       if (token) {
         localStorage.setItem('token', token);
-        return;
+        navigate('/welcome');
       }
     },
+
     onError: (err) => {
       err.graphQLErrors.forEach((error: any) => {
         console.error('Erro GraphQL:', error.message, error.name, error.code);
@@ -54,42 +60,35 @@ const Login: React.FC = () => {
   };
 
   return (
-      <form className='container-login' onSubmit={handleSubmit}>
-        <div className='box-email'>
-          <label htmlFor='email'>E-mail</label>
-          <input
-            placeholder='email@exemplo.com'
-            name='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {emailError && <p className='error-message'>{emailError}</p>}
-        </div>
-        <div className='box-password'>
-          <label htmlFor='password'>Senha</label>
-          <input
-            type='password'
-            placeholder='*****'
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {passwordError  && <p className='error-message'>{passwordError }</p>}
-        </div>
-        <div className='box-submit'>
-          <button type='submit' disabled={loading}>
-            {loading ? (
-              <>
-                <div className='custom-loader-button'></div>
-              </>
-            ) : (
-              'Entrar'
-            )}
-          </button>
-          {error && <p className='error-message'>Hola a todos, como estan? soy hyrum, sou do brasil, aqui no peru</p>}
-        </div>
-      </form>
-    </>
+    <form className='container-login' onSubmit={handleSubmit}>
+    <div className='box-email'>
+      <label htmlFor='email'>E-mail</label>
+      <input
+        placeholder='email@exemplo.com'
+        name='email'
+        value={formState.email.value}
+        onChange={(e) => updateField('email', e.target.value)}
+      />
+      {formState.email.error && <p className='error-message'>{formState.email.error}</p>}
+    </div>
+    <div className='box-password'>
+      <label htmlFor='password'>Senha</label>
+      <input
+        type='password'
+        placeholder='*****'
+        name='password'
+        value={formState.password.value}
+        onChange={(e) => updateField('password', e.target.value)}
+      />
+      {formState.password.error && <p className='error-message'>{formState.password.error}</p>}
+    </div>
+    <div className='box-submit'>
+      <button type='submit' disabled={loading}>
+        {loading ? 'Entrando...' : 'Entrar'}
+      </button>
+      {error && <p className='error-message'>{error.message}</p>}
+    </div>
+  </form>
   );
 };
 export default Login;
