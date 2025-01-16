be8fe3ae61b2118f8f546b654a07918308815a7a
devHyrum <holivera0730@gmail.com>
2025-01-16 09:54:45 -0300

    feature/styled-with-prettier

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,14 +5,16 @@ import { useNavigate } from 'react-router-dom';
 const Welcome: React.FC = () => {
   const navigate = useNavigate();
   const buttonFuction = () => {
-    navigate('/login')
-  }
+    navigate('/login');
+  };
 
   return (
     <>
       <h1>Site Instaq</h1>
       <p>Home Page</p>
-      <button className='default-button' onClick={buttonFuction}>Entrar</button>
+      <button className='default-button' onClick={buttonFuction}>
+        Entrar
+      </button>
     </>
   );
 };
